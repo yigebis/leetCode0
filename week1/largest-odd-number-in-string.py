420480d8278e742411869d9b93ca0fbe4a7cf0f2
yigebis <122978889+yigebis@users.noreply.github.com>
2023-12-12 19:18:56 +0300

    Added solution for Largest Odd Number in String

---
+++ week1/largest-odd-number-in-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        i = len(num) - 1
+        while i >= 0:
+            if int(num[i]) % 2:
+                return num[:i+1]
+            i -= 1
+
+        return "" 
