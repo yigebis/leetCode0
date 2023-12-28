c83e77265d251c55f8c09425c3ff2910a703e842
yigebis <122978889+yigebis@users.noreply.github.com>
2023-12-28 20:04:56 +0300

    Added solution for Rotate Image

---
+++ week2/rotate-image.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // transpose
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (i < j)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        // flip
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size()/2; j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].size() - 1 - j];
+                matrix[i][matrix[0].size() - 1 - j] = temp;
+            }
+        }
+    }
+};
